a1b4590c844dc7e4f283332f9049f9afcd33bcb6
= <=>
2024-12-14 16:28:15 -0300

    create task model

---
+++ src/main/java/dev/buskopan/model/Task.java
@@ -0,0 +1,47 @@
+package dev.buskopan.model;
+
+import dev.buskopan.task_enum.TaskStatus;
+
+import java.util.Objects;
+
+public class Task {
+    private long id;
+    private String name;
+    private TaskStatus status;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Task task)) return false;
+        return id == task.id && Objects.equals(name, task.name) && status == task.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, status);
+    }
+}
